e65e279c6d6a2a35f43b7c3fe4d1555b129c4b5b
Pvblitto <ruizn.5173@gmail.com>
2025-04-15 10:30:36 -0400

    input

--- Hola.py
+++ Hola.py
@@ -1,1 +1,31 @@
-print("hola mundo") 
+#variables
+print("ingrese su nombre y edad")
+NOMBRE=input()
+edad=int(input())
+# ejemplo concatenacion
+# print("hola mundo",NOMBRE,"y su edad es",edad ) 
+
+print("hola",NOMBRE,"y su edad es",edad ) 
+
+
+
+
+
+n1=int(input("ingrese 1 numero"))
+n2=int(input("ingrese 1 numero"))
+print(n1+n2)          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
